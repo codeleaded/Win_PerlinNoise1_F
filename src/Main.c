ee120d59bc08c7b3e3040ad217a1293686fb8917
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:40 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/PerlinNoise.h"
 
 
